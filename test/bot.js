7f302d72591a6c858428193807c64cd3c05758a4
Roly Fentanes <roly426@gmail.com>
2013-09-08 16:07:16 -0400

    use writestreamp

--- test/bot.js
+++ test/bot.js
@@ -1,12 +1,10 @@
-var fs       = require('fs');
+var wsp      = require('writestreamp');
 var path     = require('path');
-var mkdirp   = require('mkdirp');
 var nickserv = require('..');
 var server   = require('optimist').argv.server || null;
 
 // Mock the irc module if no server given.
 var irc = require(!server ? './mock/irc' : 'irc');
-
 
 
 /**
@@ -21,10 +19,9 @@ var irc = require(!server ? './mock/irc' : 'irc');
 module.exports = function createBot(type, nick, fn, log, options) {
   // Make server folder if it doesn't exist.
   var dir = path.join(__dirname, 'logs', server || 'none', fn);
-  mkdirp.sync(dir);
 
   // Append txt to log file for this server and command.
-  var ws = fs.createWriteStream(path.join(dir, type + '.log'));
+  var ws = wsp(path.join(dir, type + '.log'));
 
   // Create irc client instance.
   var bot = new irc.Client(server, nick, {
